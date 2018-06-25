19c884360e318b3f554815f6f6abd6c9b1d935e9
Pavel <itasarom@gmail.com>
2018-06-25 11:53:20 +0300

    Trying to tackle the overfitting

---
+++ model.py
@@ -0,0 +1,123 @@
+import numpy as np
+import torch
+import ast
+
+
+NODE_TYPES = ['AST', 'Add', 'And', 'Assert', 'Assign', 'AsyncFor', 'AsyncFunctionDef', 'AsyncWith', 'Attribute', 'AugAssign', 'AugLoad', 'AugStore', 'Await', 'BinOp', 'BitAnd', 'BitOr', 'BitXor', 'BoolOp', 'Break', 'Bytes', 'Call', 'ClassDef', 'Compare', 'Continue', 'Del', 'Delete', 'Dict', 'DictComp', 'Div', 'Ellipsis', 'Eq', 'ExceptHandler', 'Expr', 'Expression', 'ExtSlice', 'FloorDiv', 'For', 'FunctionDef', 'GeneratorExp', 'Global', 'Gt', 'GtE', 'If', 'IfExp', 'Import', 'ImportFrom', 'In', 'Index', 'Interactive', 'Invert', 'Is', 'IsNot', 'LShift', 'Lambda', 'List', 'ListComp', 'Load', 'Lt', 'LtE', 'MatMult', 'Mod', 'Module', 'Mult', 'Name', 'NameConstant', 'NodeTransformer', 'NodeVisitor', 'Nonlocal', 'Not', 'NotEq', 'NotIn', 'Num', 'Or', 'Param', 'Pass', 'Pow', 'PyCF_ONLY_AST', 'RShift', 'Raise', 'Return', 'Set', 'SetComp', 'Slice', 'Starred', 'Store', 'Str', 'Sub', 'Subscript', 'Suite', 'Try', 'Tuple', 'UAdd', 'USub', 'UnaryOp', 'While', 'With', 'Yield', 'YieldFrom', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'alias', 'arg', 'arguments', 'boolop', 'cmpop', 'comprehension', 'copy_location', 'dump', 'excepthandler', 'expr', 'expr_context', 'fix_missing_locations', 'get_docstring', 'increment_lineno', 'iter_child_nodes', 'iter_fields', 'keyword', 'literal_eval', 'mod', 'operator', 'parse', 'slice', 'stmt', 'unaryop', 'walk', 'withitem']
+
+
+class EmbeddingVisitor(ast.NodeVisitor):
+    def __init__(self, embedding_layer, subtree_network, embedding_network):
+        super(self.__class__, self).__init__()
+        self.embedding_dim = embedding_layer.embedding_dim
+        self.embedding_layer = embedding_layer
+        self.subtree_network = subtree_network
+
+        self.embedding_network = embedding_network
+        self.mapping = {name : index for index, name in enumerate(NODE_TYPES)}
+
+    def visit(self, node):
+        return self.embed_node(node)
+
+    def node_to_index(self, node):
+        return self.mapping[node.__class__.__name__]
+
+    def embed_node_proper(self, node):
+        tmp = np.array([self.node_to_index(node)])
+        # print(tmp)
+        return self.embedding_layer(torch.from_numpy(tmp))
+
+    def embed_node(self, node):
+        node_embedding = self.embed_node_proper(node)
+        if len(list(ast.iter_child_nodes(node))):
+            embeddings = [node_embedding]
+            embeddings += self.embed_subtree(node)
+            # result = self.embedding_network(torch.cat((node_embedding, subtree_embedding), -1))
+            c_0 = torch.zeros(1, self.embedding_dim)
+            h_0 = torch.zeros(1, self.embedding_dim)
+
+            # print(len(embeddings))
+            for node_embedding in embeddings:
+                (c_0, h_0) = self.subtree_network(node_embedding, (c_0, h_0))
+
+            return c_0
+
+        return node_embedding
+
+    def embed_subtree(self, node):
+        children_embeddings = []
+        # c_0 = torch.zeros(1, self.embedding_dim)  
+        # h_0 = torch.zeros(1, self.embedding_dim)
+        # n_children = 0
+
+        for child in ast.iter_child_nodes(node):
+            # print(child)
+            child_embedding = self.embed_node(child).view(1, -1)
+            children_embeddings.append(child_embedding)
+            # (c_0, h_0) = self.subtree_network(child_embedding, (c_0, h_0))
+            # h_0 += child_embedding
+            # n_children += 1
+
+        # h_0 /= n_children
+        # indices = torch.from_numpy()
+        # embeddings = self.embedding_layer(indices)
+        
+        # children_embeddings = torch.cat(children_embeddings, 0)[None, :, :]
+        # print(children_embeddings)
+        
+        return children_embeddings
+
+
+class ASTEncoder(torch.nn.Module):
+    def __init__(self, embedding_dims=100):
+        super(self.__class__, self).__init__()
+        n_nodes = len(NODE_TYPES)
+        self.embedding_dims = embedding_dims
+        self.subtree_network = torch.nn.LSTMCell(embedding_dims, embedding_dims, dropout=0.2)
+        #self.embedding_network = torch.nn.Sequential(
+        #                        torch.nn.Linear(2 * embedding_dims, 256),
+        #                        torch.nn.ReLU(),
+        #                        torch.nn.Linear(256, embedding_dims)
+        #                    )
+
+        self.embedding_layer = torch.nn.Embedding(n_nodes, embedding_dims)
+
+        self.visitor = EmbeddingVisitor(self.embedding_layer, self.subtree_network, None)
+
+    def forward(self, node):
+        return self.visitor.visit(node)
+
+
+class Model(torch.nn.Module):
+    def __init__(self, n_classes):
+        super(self.__class__, self).__init__()
+        self.ast_encoder = ASTEncoder()
+        self.softmax_head = torch.nn.Sequential(
+                        torch.BatchNorm1d(self.self.ast_encoder.embedding_dims),
+                        torch.nn.Linear(self.ast_encoder.embedding_dims, 256),
+                        torch.nn.ReLU(),
+                        torch.nn.Linear(256, n_classes),
+                        # torch.nn.Softmax(dim=1)
+                    )
+
+
+
+    def transform_batch(self, input):
+        result = torch.zeros(len(input), self.ast_encoder.embedding_dims)
+
+        for id, code in enumerate(input):
+            result[id] = self.ast_encoder(ast.parse(code))
+
+        return result
+
+    def forward(self, input):
+
+        embeddings = self.transform_batch(input)
+
+        # print(embeddings)
+
+        result = self.softmax_head(embeddings)
+
+        return result
+
+
---
+++ train.py
@@ -0,0 +1,205 @@
+import tqdm
+import numpy as np
+import torch
+#import datetime
+#from launch_utils import logger
+from collections import defaultdict
+from sklearn.metrics import log_loss, accuracy_score
+import matplotlib.pyplot as plt
+from IPython import display
+
+import os
+from collections import defaultdict
+def read_all(path = "../cf/Solutions"):
+    result = {}
+    for handle in os.listdir(path):
+        handle_path = os.path.join(path, handle)
+        result_for_handle = defaultdict(list)
+        for contest in os.listdir(handle_path):
+            contest_path = os.path.join(handle_path, contest)
+            for solution in os.listdir(contest_path):
+                solution_path = os.path.join(contest_path, solution)
+                with open(solution_path, "r") as f:
+                    result_for_handle[contest + ":" + solution] = f.read()
+                    
+        result[handle] = result_for_handle
+        
+    return result
+
+
+class Batcher:
+    def __init__(self, data, batch_size):
+        np.random.seed(42)
+
+        self.batch_size = batch_size
+        self.data = data
+        self.classes = list(sorted(data.keys()))
+
+        self.class_encoder = {handle:id for id, handle in enumerate(self.classes)}
+
+
+        self.y = []
+        self.x = []
+        for handle, submissions in data.items():
+            for submission, src in submissions.items():
+                self.y.append(self.class_encoder[handle])
+                self.x.append(src)
+
+        self.y = np.array(self.y)
+        self.x = np.array(self.x)
+
+        order = np.random.permutation(np.arange(len(self.x)))
+
+        self.y = self.y[order]
+        self.x = self.x[order]
+
+    def get_n_classes(self):
+        return len(self.classes)
+
+
+class NameBatcher:
+    def __init__(self, data, batch_size, train_names, test_names):
+        np.random.seed(42)
+
+        self.batch_size = batch_size
+        self.data = data
+        self.classes = list(sorted(data.keys()))
+
+        self.class_encoder = {handle:id for id, handle in enumerate(self.classes)}
+
+
+        self.y_train = []
+        self.x_train = []
+        self.y_test = []
+        self.x_test = []
+
+        for handle, submissions in data.items():
+            for submission, src in submissions.items():
+                if submission in train_names:
+                    self.y_train.append(self.class_encoder[handle])
+                    self.x_train.append(src)
+                elif submission in test_names:
+                    self.y_test.append(self.class_encoder[handle])
+                    self.x_test.append(src)
+
+        self.y_train = np.array(self.y_train)
+        self.x_train = np.array(self.x_train)
+
+        order = np.random.permutation(np.arange(len(self.x_train)))
+
+        self.y_train = self.y_train[order]
+        self.x_train = self.x_train[order]
+
+        self.y_test = np.array(self.y_test)
+        self.x_test = np.array(self.x_test)
+
+    def get_n_classes(self):
+        return len(self.classes)
+
+
+
+    def train(self):
+        for pos in range(0, len(self.x_train), self.batch_size):
+            yield self.x_train[pos:pos + self.batch_size], torch.from_numpy(np.array(self.y_train[pos:pos + self.batch_size], dtype=np.int64))
+
+    def test(self):
+        for pos in range(0, len(self.x_test), self.batch_size):
+            yield self.x_test[pos:pos + self.batch_size], torch.from_numpy(np.array(self.y_test[pos:pos + self.batch_size], dtype=np.int64))
+
+
+
+
+class Trainer:
+    def __init__(self, model, loss_object, optimizer):
+
+        torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)
+        self.model = model
+        self.loss_object = loss_object
+        self.optimizer = optimizer
+                
+        self.train_metrics = defaultdict(list)
+        self.validation_metrics = defaultdict(list)
+        self.all_params = {}
+        
+    def save(self, path):
+        torch.save(self.model.state_dict(), path)
+
+                
+    def restore(self, path):
+        if self.global_iterations > 0:
+            raise ValueError(
+                "Cannot restore variables to an already trained model! (Trained for {} global iterations)"\
+                    .format(self.global_iterations))
+            
+        self.model.load_state_dict(torch.load(path))
+
+
+    
+    def log_global_iteration(self):
+        pass
+
+
+    def validate(self):
+        pass
+
+
+
+    
+    def train(self, batch_sampler, params):
+        self.model.train()
+        for epoch_id in range(params['n_epochs']):
+            self.model.train()
+            for x, y in batch_sampler.train():
+                # print(y)
+                self.optimizer.zero_grad()
+                prediction = self.model(x)
+                # print(prediction)
+                loss = self.loss_object(prediction, y)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.25)
+                self.optimizer.step()
+                self.train_metrics['loss'].append(loss.detach().numpy())
+
+                display.clear_output(wait=True)
+                plt.plot(self.train_metrics['loss'], color='red', label='train')
+                plt.plot(self.validation_metrics['validation_iterations'], self.validation_metrics['loss'], color='blue', label='val')
+                plg.legend()
+                plt.show()
+
+                plt.plot(self.train_metrics['accuracy'], color='red', label='train')
+                plt.plot(self.validation_metrics['validation_iterations'], self.validation_metrics['accuracy'], color='blue', label='val')
+                plg.legend()
+                plt.show()
+                # print(y)
+                # print(prediction)
+
+            print(self.train_metrics['loss'][-1])
+
+            self.model.eval()
+            z = 0
+            n_items = 0
+            for x, y in batch_sampler.train():
+                prediction = self.model(x)
+                _, prediction = prediction.max(dim=1)
+                z += np.count_nonzero(prediction == y)
+                n_items += len(prediction)
+
+            self.train_metrics['accuracy'].append(z/n_items)
+            print(self.train_metrics['accuracy'][-1])
+
+
+            z = 0
+            n_items = 0
+            loss_acc = 0
+            for x, y in batch_sampler.test():
+                prediction = self.model(x)
+                loss_acc += self.loss_object(prediction, y).detach().numpy() * len(x)
+                _, prediction = prediction.max(dim=1)
+                z += np.count_nonzero(prediction == y)
+                n_items += len(prediction)
+
+            self.validation_metrics['validation_iterations'].append(len(self.train_metrics['loss']))
+            self.validation_metrics['loss'].append(loss_acc/n_items)
+            self.validation_metrics['accuracy'].append(z/n_items)
+            print(self.validation_metrics['accuracy'][-1])
+
